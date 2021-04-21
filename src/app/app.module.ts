2b5be8f0ab61d1d8772b77e6877b52277102774b
*Adin <*adin.lopez.mun@gmail.com*>
2021-04-21 15:55:34 -0400

    subiendo la actividad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeadComponent } from './head/head.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
